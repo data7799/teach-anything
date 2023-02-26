6a349d3683230afc99ef3590b9c202d51cc78462
walker0001 <changxin2090@gmail.com>
2023-02-27 05:16:45 +0800

    更新 generate.ts
    调高token

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -26,7 +26,7 @@ const handler = async (req: NextRequest): Promise<Response> => {
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 500,
+    max_tokens: 2048,
     stream: true,
     n: 1,
   };
